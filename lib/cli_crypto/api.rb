1b37fe0eecd1a2bd0614263ed6848acf475a8040
Brandon <sugb73@gmail.com>
2021-02-26 11:08:28 -0500

    Refactored the fetch method

--- lib/cli_crypto/api.rb
+++ lib/cli_crypto/api.rb
@@ -12,24 +12,26 @@ class CliCrypto::Api
         
         # create a instance of CryptoCurrency with the bitcoin response
 
-        btc_instance = CliCrypto::CryptoCurrency.new(
+        array << CliCrypto::CryptoCurrency.new(
             "Bitcoin",
             response_btc["ticker"]["price"],
             response_btc["ticker"]["change"],
-            response_btc["ticker"]["volume"]
-        )
+            response_btc["ticker"]["volume"])
 
-        ltc_instance = CliCrypto::CryptoCurrency.new("Litecoin", response_ltc["ticker"]["price"], response_ltc["ticker"]["change"], response_ltc["ticker"]["volume"])
+        array << CliCrypto::CryptoCurrency.new("Litecoin",
+            response_ltc["ticker"]["price"],
+            response_ltc["ticker"]["change"],
+            response_ltc["ticker"]["volume"])
         
-        doge_instance = CliCrypto::CryptoCurrency.new("Dogecoin", response_doge["ticker"]["price"], response_doge["ticker"]["change"], response_doge["ticker"]["volume"])
+        array << CliCrypto::CryptoCurrency.new("Dogecoin",
+            response_doge["ticker"]["price"],
+            response_doge["ticker"]["change"],
+            response_doge["ticker"]["volume"])
         
-        etc_instance = CliCrypto::CryptoCurrency.new("Ethereum", response_etc["ticker"]["price"], response_etc["ticker"]["change"], response_etc["ticker"]["volume"])
-
-        # add instance to an array
-        array << btc_instance
-        array << ltc_instance
-        array << doge_instance
-        array << etc_instance
+        array << CliCrypto::CryptoCurrency.new("Ethereum",
+            response_etc["ticker"]["price"],
+            response_etc["ticker"]["change"],
+            response_etc["ticker"]["volume"])
 
         # return an array that has all the instances
         return array
